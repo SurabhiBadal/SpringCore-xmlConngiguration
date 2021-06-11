61def213baf3f50549f2e491619404cbd7bc78ec
SurabhiBadal <s.badal2012@gmail.com>
2021-06-12 02:29:09 +0530

    IOC with xml configuration

--- src/BaseBallCoach.java
+++ src/BaseBallCoach.java
@@ -2,6 +2,6 @@ public class BaseBallCoach implements Coach {
 
     @Override
     public String getDailyRoutine() {
-        return "Practise for 90 minutes";
+        return "Practise baseball for 90 minutes";
     }
 }
