77827cff69644e3e94e9b5217b30052908c15ca6
SurabhiBadal <s.badal2012@gmail.com>
2021-06-13 12:44:24 +0530

    Performed literal injection

--- src/CricketCoach.java
+++ src/CricketCoach.java
@@ -1,14 +1,35 @@
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String emailAddress;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    private String team;
 
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+
     @Override
     public String getDailyRoutine() {
         return "Practice Cricket for 120 minutes";
     }
+
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
--- src/SpringApp.java
+++ src/SpringApp.java
@@ -1,17 +1,23 @@
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.SortedMap;
+
 public class SpringApp {
     public static void main(String[] args) {
         //Load the Spring configuration file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //Retrieve bean from container
-        Coach theCoach=context.getBean("myCricketCoach",Coach.class);
+        CricketCoach theCoach=context.getBean("myCricketCoach",CricketCoach.class);
         // call methods on the bean
         System.out.println(theCoach.getDailyRoutine());
 
         // Call the new method
         System.out.println(theCoach.getDailyFortune());
+
+        // call the get email and get team
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
 
         //close the context
         context.close();
